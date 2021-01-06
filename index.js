3a48ea5ce26ba50a05f80e1c4d878f7b7f981e8e
Markus Michel <nemocadneza@googlemail.com>
2021-01-06 12:05:36 +0100

    refactoring: removed support for multiple lametric instances

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(`Starting Pi-Hole for LaMetric ${config.version}...`);
 let spinner = ora(
   `Testing Pi-Hole Connection @ ${config.PiHole.IP}...`
 ).start();
-let availableLaMetrics = [];
+let init = false;
 
 if (config.debugMode) {
   console.log("Debug Mode Enabled");
@@ -21,29 +21,29 @@ fetch(
     if (piHoleRes.querytypes != null) {
       spinner.succeed(`Pi-Hole Auth Valid!`);
       let successfulLaMetricConnections = 0;
-      let laMetricTest = (indexNumber) => {
-        if (config.LaMetric[indexNumber] != null) {
+      let laMetricTest = () => {
+        if (init == false) {
           spinner = ora(
-            `Testing Connection to LaMetric @ ${config.LaMetric[indexNumber].IP}...`
+            `Testing Connection to LaMetric @ ${config.LaMetric.IP}...`
           ).start();
           let laMetricAuthValue = `Basic ${Buffer.from(
-            `dev:${config.LaMetric[indexNumber].AuthKey}`
+            `dev:${config.LaMetric.AuthKey}`
           ).toString("base64")}`;
           fetchWithAuth(
-            `http://${config.LaMetric[indexNumber].IP}:8080/api/v2/device/apps/com.lametric.58091f88c1c019c8266ccb2ea82e311d`,
+            `http://${config.LaMetric.IP}:8080/api/v2/device/apps/com.lametric.58091f88c1c019c8266ccb2ea82e311d`,
             laMetricAuthValue
           )
             .then((laMetricDeviceInfo) => {
               successfulLaMetricConnections++;
               fetchWithAuth(
-                `http://${config.LaMetric[indexNumber].IP}:8080/api/v2/device`,
+                `http://${config.LaMetric.IP}:8080/api/v2/device`,
                 laMetricAuthValue
               ).then((laMetricDeviceInfo2) => {
                 spinner.succeed(
-                  `Connected to "${laMetricDeviceInfo2.name}" @ ${config.LaMetric[indexNumber].IP} running OS v${laMetricDeviceInfo2.os_version} & Pi-Hole Status v${laMetricDeviceInfo.version}! (${laMetricDeviceInfo2.serial_number})`
+                  `Connected to "${laMetricDeviceInfo2.name}" @ ${config.LaMetric.IP} running OS v${laMetricDeviceInfo2.os_version} & Pi-Hole Status v${laMetricDeviceInfo.version}! (${laMetricDeviceInfo2.serial_number})`
                 );
-                availableLaMetrics.push(config.LaMetric[indexNumber]);
-                laMetricTest(indexNumber + 1);
+                init = true;
+                laMetricTest();
               });
             })
             .catch((err) => {
@@ -53,134 +53,121 @@ fetch(
               if (err.statusCode != null && err.body.errors != null) {
                 if (err.statusCode == 401) {
                   spinner.fail(
-                    `Connection to LaMetric @ ${config.LaMetric[indexNumber].IP} Failed. Auth invalid.`
+                    `Connection to LaMetric @ ${config.LaMetric.IP} Failed. Auth invalid.`
                   );
                 } else if (err.statusCode == 404) {
                   spinner.fail(
-                    `Connection to LaMetric @ ${config.LaMetric[indexNumber].IP} Failed. Pi-Hole Status app not installed on the LaMetric.`
+                    `Connection to LaMetric @ ${config.LaMetric.IP} Failed. Pi-Hole Status app not installed on the LaMetric.`
                   );
                 } else {
                   spinner.fail(
-                    `Connection to LaMetric @ ${config.LaMetric[indexNumber].IP} Failed. LaMetric does not seem to linked to this IP.`
+                    `Connection to LaMetric @ ${config.LaMetric.IP} Failed. LaMetric does not seem to linked to this IP.`
                   );
                 }
               } else {
                 spinner.fail(
-                  `Connection to LaMetric @ ${config.LaMetric[indexNumber].IP} Failed. LaMetric does not seem to linked to this IP.`
+                  `Connection to LaMetric @ ${config.LaMetric.IP} Failed. LaMetric does not seem to linked to this IP.`
                 );
               }
-              laMetricTest(indexNumber + 1);
+              laMetricTest();
             });
         } else {
-          if (successfulLaMetricConnections > 0) {
-            let updateLaMetric = () => {
-              var piHoleCalls = [
-                fetch(
-                  `http://${config.PiHole.IP}/admin/api.php?summary&auth=${config.PiHole.AuthKey}`
-                ).then((res) => res.json()),
-                fetch(
-                  `http://${config.PiHole.IP}/admin/api.php?topItems&auth=${config.PiHole.AuthKey}`
-                ).then((res) => res.json()),
-                fetch(
-                  `http://${config.PiHole.IP}/admin/api.php?recentBlocked&auth=${config.PiHole.AuthKey}`
-                ).then((res) => res.text()),
-              ];
-              Promise.all(piHoleCalls).then(function ([
-                piHoleSummaryData,
-                piHoleTopItemsData,
-                piHoleRecentBlockedData,
-              ]) {
-                let topQueryArray = Object.values(
+          let updateLaMetric = () => {
+            var piHoleCalls = [
+              fetch(
+                `http://${config.PiHole.IP}/admin/api.php?summary&auth=${config.PiHole.AuthKey}`
+              ).then((res) => res.json()),
+              fetch(
+                `http://${config.PiHole.IP}/admin/api.php?topItems&auth=${config.PiHole.AuthKey}`
+              ).then((res) => res.json()),
+              fetch(
+                `http://${config.PiHole.IP}/admin/api.php?recentBlocked&auth=${config.PiHole.AuthKey}`
+              ).then((res) => res.text()),
+            ];
+            Promise.all(piHoleCalls).then(function ([
+              piHoleSummaryData,
+              piHoleTopItemsData,
+              piHoleRecentBlockedData,
+            ]) {
+              let topQueryArray = Object.values(piHoleTopItemsData.top_queries);
+              let topBlockedQueryArray = Object.values(
+                piHoleTopItemsData.top_ads
+              );
+              let body = {
+                blockListSize: piHoleSummaryData.domains_being_blocked,
+                dnsQueriesToday: piHoleSummaryData.dns_queries_today,
+                adsBlockedToday: piHoleSummaryData.ads_blocked_today,
+                totalClientsSeen: piHoleSummaryData.clients_ever_seen,
+                totalDNSQueries: piHoleSummaryData.dns_queries_all_types,
+                topQuery: `${Object.keys(
                   piHoleTopItemsData.top_queries
-                );
-                let topBlockedQueryArray = Object.values(
+                )[0].toString()} (${topQueryArray[0].toString()} Queries)`,
+                topBlockedQuery: `${Object.keys(
                   piHoleTopItemsData.top_ads
-                );
-                let body = {
-                  blockListSize: piHoleSummaryData.domains_being_blocked,
-                  dnsQueriesToday: piHoleSummaryData.dns_queries_today,
-                  adsBlockedToday: piHoleSummaryData.ads_blocked_today,
-                  totalClientsSeen: piHoleSummaryData.clients_ever_seen,
-                  totalDNSQueries: piHoleSummaryData.dns_queries_all_types,
-                  topQuery: `${Object.keys(
-                    piHoleTopItemsData.top_queries
-                  )[0].toString()} (${topQueryArray[0].toString()} Queries)`,
-                  topBlockedQuery: `${Object.keys(
-                    piHoleTopItemsData.top_ads
-                  )[0].toString()} (${topBlockedQueryArray[0].toString()} Queries)`,
-                  lastBlockedQuery: piHoleRecentBlockedData,
-                };
+                )[0].toString()} (${topBlockedQueryArray[0].toString()} Queries)`,
+                lastBlockedQuery: piHoleRecentBlockedData,
+              };
 
-                availableLaMetrics.forEach((LaMetric) => {
-                  let updateIndex = 1;
-                  let updateSpinner = ora(
-                    `Connecting to LaMetric @ ${LaMetric.IP}...`
-                  ).start();
+              let updateSpinner = ora(
+                `Connecting to LaMetric @ ${config.LaMetric.IP}...`
+              ).start();
+              fetchWithAuth(
+                `http://${config.LaMetric.IP}:8080/api/v2/device/apps/com.lametric.58091f88c1c019c8266ccb2ea82e311d`,
+                `Basic ${Buffer.from(`dev:${config.LaMetric.AuthKey}`).toString(
+                  "base64"
+                )}`
+              )
+                .then((laMetricDeviceInfo) => {
                   fetchWithAuth(
-                    `http://${LaMetric.IP}:8080/api/v2/device/apps/com.lametric.58091f88c1c019c8266ccb2ea82e311d`,
-                    `Basic ${Buffer.from(`dev:${LaMetric.AuthKey}`).toString(
-                      "base64"
-                    )}`
-                  )
-                    .then((laMetricDeviceInfo) => {
-                      fetchWithAuth(
-                        `http://${LaMetric.IP}:8080/api/v2/device`,
-                        `Basic ${Buffer.from(
-                          `dev:${LaMetric.AuthKey}`
-                        ).toString("base64")}`
-                      ).then((laMetricDeviceInfo2) => {
-                        updateSpinner.text = `Sending update for "${laMetricDeviceInfo2.name}" @ ${LaMetric.IP} to the server...`;
-                        fetch(
-                          `https://lametric.iderp.io/pihole/${laMetricDeviceInfo2.id}`,
-                          {
-                            method: "POST",
-                            body: body,
-                          }
-                        ).then(() => {
-                          updateIndex++;
-                          updateSpinner.succeed(
-                            `Sent update for "${laMetricDeviceInfo2.name}" @ ${LaMetric.IP} to the server!`
-                          );
-                        });
-                      });
-                    })
-                    .catch((err) => {
-                      if (config.debugMode) {
-                        console.log(err);
+                    `http://${config.LaMetric.IP}:8080/api/v2/device`,
+                    `Basic ${Buffer.from(
+                      `dev:${config.LaMetric.AuthKey}`
+                    ).toString("base64")}`
+                  ).then((laMetricDeviceInfo2) => {
+                    updateSpinner.text = `Sending update for "${laMetricDeviceInfo2.name}" @ ${config.LaMetric.IP} to the server...`;
+                    fetch(
+                      `https://lametric.iderp.io/pihole/${laMetricDeviceInfo2.id}`,
+                      {
+                        method: "POST",
+                        body: body,
                       }
-                      if (err.statusCode != null && err.body.errors != null) {
-                        if (err.statusCode == 401) {
-                          updateSpinner.fail(
-                            `Update failed to send for LaMetric @ ${LaMetric.IP}. Auth invalid.`
-                          );
-                        } else if (err.statusCode == 404) {
-                          updateSpinner.fail(
-                            `Update failed to send for LaMetric @ ${LaMetric.IP}. Pi-Hole Status app not installed on the LaMetric.`
-                          );
-                        } else {
-                          updateSpinner.fail(
-                            `Update failed to send for LaMetric @ ${LaMetric.IP}. LaMetric does not seem to linked to this IP.`
-                          );
-                        }
-                      } else {
-                        updateSpinner.fail(
-                          `Update failed to send for LaMetric @ ${LaMetric.IP}. LaMetric does not seem to linked to this IP.`
-                        );
-                      }
+                    ).then(() => {
+                      updateSpinner.succeed(
+                        `Sent update for "${laMetricDeviceInfo2.name}" @ ${config.LaMetric.IP} to the server!`
+                      );
                     });
+                  });
+                })
+                .catch((err) => {
+                  if (config.debugMode) {
+                    console.log(err);
+                  }
+                  if (err.statusCode != null && err.body.errors != null) {
+                    if (err.statusCode == 401) {
+                      updateSpinner.fail(
+                        `Update failed to send for LaMetric @ ${config.LaMetric.IP}. Auth invalid.`
+                      );
+                    } else if (err.statusCode == 404) {
+                      updateSpinner.fail(
+                        `Update failed to send for LaMetric @ ${config.LaMetric.IP}. Pi-Hole Status app not installed on the LaMetric.`
+                      );
+                    } else {
+                      updateSpinner.fail(
+                        `Update failed to send for LaMetric @ ${config.LaMetric.IP}. LaMetric does not seem to linked to this IP.`
+                      );
+                    }
+                  } else {
+                    updateSpinner.fail(
+                      `Update failed to send for LaMetric @ ${config.LaMetric.IP}. LaMetric does not seem to linked to this IP.`
+                    );
+                  }
                 });
-              });
-            };
-            setInterval(() => {
-              updateLaMetric();
-            }, config.updateInterval * 1000);
+            });
+          };
+          setInterval(() => {
             updateLaMetric();
-          } else {
-            console.log(
-              `At least 1 LaMetric must have a successful connection to continue.`
-            );
-            process.exit();
-          }
+          }, config.updateInterval * 1000);
+          updateLaMetric();
         }
       };
       laMetricTest(0);
